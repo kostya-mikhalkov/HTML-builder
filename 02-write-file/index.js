da575501d4e592e980b3f6c8e81aa457e491aa40
Kostya <kostya.mikhalkov@bk.ru>
2024-01-19 22:17:46 +0300

    refactor: changed the file name text.txt -> 02-write-file.txt

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,10 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 
-const filePath = path.join(__dirname, 'text.txt');
+const filePath = path.join(__dirname, '02-write-file.txt');
 const writeStream = fs.createWriteStream(filePath, 'utf8');
 
 const readlineInterface = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+console.log('Hello my friend');
